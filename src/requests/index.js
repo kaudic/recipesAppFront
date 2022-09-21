8a5d75d263dfc096f8e4325d15ef909cea24717f
Killian Audic <killianaudic@aol.fr>
2022-09-21 15:09:20 +0200

    added url for making production requests to API

--- src/requests/index.js
+++ src/requests/index.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
+
+
 // on créer une instance axios pour notre API
 const apiAxios = axios.create({
   // Attention bien mettre http:// sinon souci (obscure) de cors
-  baseURL: `${process.env.REACT_APP_BASE_URL}/api`,
+  baseURL: process.env.NODE_ENV === 'production' ? `${process.env.REACT_APP_BASE_URL_PROD}/api` : `${process.env.REACT_APP_BASE_URL_DEV}/api`
 });
 export default apiAxios;
