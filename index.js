028749bf7a71eb7c7117092a14b15c85457a2999
Killian Audic <killianaudic@aol.fr>
2022-09-21 15:45:37 +0200

    added a console log to check if request received - delete the route

--- index.js
+++ index.js
@@ -5,11 +5,17 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT ?? 3002;
 
+app.use((req, res, next) => {
+    console.log('request received in recipesAppFront - trying to serve react static file1');
+    next();
+});
+
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get('/', async (req, res) => {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
-});
+// app.get('/', async (req, res) => {
+//     console.log('request received in recipesAppFront - trying to serve react static file2');
+//     res.sendFile(path.join(__dirname, "build", "index.html"));
+// });
 
 // app listenning to port
 app.listen(port, () => {
